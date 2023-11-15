39935d5888112619bc2778bde7e630589482b76e
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-11-15 19:59:13 +0100

    es3

--- es1.ts
+++ es1.ts
@@ -17,11 +17,19 @@ const setupDb = () => {
           rating INT,
           stock_count INT
       );
-    ALTER TABLE books
-    ADD COLUMN publisher TEXT NOT NULL;
-    ALTER TABLE books  
-    ADD COLUMN number_of_pages INTEGER 
       `);
+  database.none(
+    `INSERT INTO books VALUES ('1', 'A Journey to the Center','Jules Verne','Adventure','1864', '123456789','12.99','4','10','Verne Publishing','350')`
+  );
+  database.none(
+    `INSERT INTO books VALUES ('2', 'War And Peace','Lev Tolstoy','Historical','1869', '123456790','14.99','5','5','Tolstoy Prints','1200')`
+  );
+  database.none(
+    `INSERT INTO books VALUES ('3', 'Whispers of the Wind','Amelia Blackburn','Romance','1982', '123456791','9.99','4','20','BlackBourn House','275')`
+  );
+  database.none(
+    `INSERT INTO books VALUES ('4', 'The Galactic Odyssey','Orion Starfield','Science Fiction','2005', '123456792','19.99','5','15','Nebula Press','450')`
+  );
 };
 
 setupDb();
